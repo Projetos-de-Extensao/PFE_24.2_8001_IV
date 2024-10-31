10af8c577689febd59a9650c2f39542f11d81d8b
GuilhermeLopes06 <guilhermedemoraes54@gmail.com>
2024-10-31 10:54:28 -0300

    autoplay speed

--- ccxp-guide/src/components/CarrosselCcxp.jsx
+++ ccxp-guide/src/components/CarrosselCcxp.jsx
@@ -18,7 +18,7 @@ function CarrosselCcxp() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 1000,
     fade: true,
     pauseOnHover: true,
   };
