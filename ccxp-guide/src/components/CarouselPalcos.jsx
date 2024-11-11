f05cb37f66e3273b721bc5849d1241d3b913eb2d
GuilhermeLopes06 <guilhermedemoraes54@gmail.com>
2024-11-11 11:36:33 -0300

    carrossel palcos responsivo

--- ccxp-guide/src/components/CarouselPalcos.jsx
+++ ccxp-guide/src/components/CarouselPalcos.jsx
@@ -1,4 +1,3 @@
-// src/components/CarouselPalcos.js
 import React from "react";
 import { motion } from "framer-motion";
 import '../index.css';
@@ -7,27 +6,26 @@ import Imagem2 from '../media/img/palco-omelete.png';
 import Imagem3 from '../media/img/palco-creators.png';
 
 const stages = [
-  { id: 1, image: Imagem1, title: "Palco Principal", subtitle: "O coração do evento" },
-  { id: 2, image: Imagem2, title: "Palco Arena", subtitle: "Convidados especiais" },
-  { id: 3, image: Imagem3, title: "Palco Experiência", subtitle: "Interação com os fãs" },
+  { id: 1, imagem: Imagem1, titulo: "Palco Principal", subtitulo: "O coração do evento" },
+  { id: 2, imagem: Imagem2, titulo: "Palco Arena", subtitulo: "Convidados especiais" },
+  { id: 3, imagem: Imagem3, titulo: "Palco Experiência", subtitulo: "Interação com os fãs" },
 ];
 
-const CarouselPalcos = () => {
-  // Define o limite de arraste com base na largura da tela
-  const dragConstraints = window.innerWidth < 768 ? -450 : -((stages.length - 1) * 300);
 
+
+const CarouselPalcos = () => {
   return (
     <div className="carousel-palcos">
-      <motion.div
-        className="carousel"
-        drag="x"
-        dragConstraints={{ right: 0, left: dragConstraints }}
+      <motion.div 
+        className="carousel" 
+        drag="x" 
+        dragConstraints={{ right: 0, left: -((stages.length - 1) * 100) + '%' }} // Ajustado para 100% da largura
       >
         {stages.map((stage) => (
           <motion.div className="carousel-item" key={stage.id}>
-            <img src={stage.image} alt={stage.title} className="stage-image" draggable="false" />
-            <h3>{stage.title}</h3>
-            <p>{stage.subtitle}</p>
+            <img src={stage.imagem} alt={stage.titulo} className="stage-image" draggable="false" />
+            <h3>{stage.titulo}</h3>
+            <p>{stage.subtitulo}</p>
           </motion.div>
         ))}
       </motion.div>
